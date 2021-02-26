85612c2cf60bec2dba82dbcaa29ef3a860eb7b8d
Lynnibinny <72465633+Lynnibinny@users.noreply.github.com>
2021-02-26 09:46:45 +0100

    hi

--- lib/insertdata.php
+++ lib/insertdata.php
@@ -1,56 +1,9 @@
 <?php
-/*
 error_reporting(E_ALL);
 include "config.php";
 // REGISTER USER
 
-  echo "yea <br/>"; 
-$ProMail = "ggjlfhgg@me.com";
-$ProUserName= "lele";
-$ProPicture="00110";
-$ProSavedTrees="4";
-$ProDonated="5.5";
-$ProPassword="ililil";
-
-error_log("saluuuut",3,"/var/www/i-kf.ch/SaveATree/lib/php.log");
-echo(heyyyy);
-if ('POST' == $_SERVER['REQUEST_METHOD']) {
-    echo(haha);
-    if (!isset($_POST['ProMail'], $_POST['ProUserName'], $_POST['ProPicture'], $_POST['ProSavedTrees'], $_POST['ProDonated'], $_POST['ProPassword'])) {
-        die ('Benutzen sie nur Formulare von der Homepage.');
-    }
-    if (('' == $ProMail = $_POST['ProMail']) or 
-           ('' == $ProUserName = $_POST['ProUserName']) or
-           ('' == $ProPicture = ($_POST['ProPicture']) or
-           ('' == $ProSavedTrees = $_POST['ProSavedTrees']) or
-           ('' == $ProDonated = $_POST['ProDonated']) or
-           ('' == $ProPassword = $_POST['ProPassword']) 
-           )) {
-       die ('Bitte füllen sie das Formular vollständig aus.');
-   }
-    $sql = "INSERT INTO TProfile (ProMail,ProUserName, ProPicture, ProSavedTrees, ProDonated, ProPassword)
-  			  VALUES(?,?,?,?,?,?)"; //,ProPicture,ProSavedTrees,ProDonated,ProPassword $ProUserName,$ProPicture,$ProSavedTrees,$ProDonated,$ProPassword')";
-       echo(eeeee);         
-    $stmt = $db->prepare($sql);
-   // $stmt->execute(array($ProMail,$ProUserName));
-   $stmt-> bind_param("ssssss",$ProMail, $ProUserName, $ProPicture, $ProSavedTrees, $ProDonated, $ProPassword);
-    if(!$stmt) {
-        die('error');
-    } 
-    if(!$stmt->execute()) {
-        printf("Error: %s.\n", $stmt->error);
-        die('error 1');
-    }  
-    
-     echo "hallo";
-    
-}*/
-
-error_reporting(E_ALL);
-include "config.php";
-// REGISTER USER
-
-  echo "yea <br/>"; 
+echo "yea <br/>"; 
 $ProMail = "liiiit@me.com";
 $ProUserName= "lele";
 $ProPicture="00110";
@@ -74,11 +27,12 @@ error_log("saluuuut",3,"/var/www/i-kf.ch/SaveATree/lib/php.log");
            )) {
        die ('Bitte füllen sie das Formular vollständig aus.');
    }*/
+    $obj = json_decode($ProMail);
     $sql = "INSERT INTO TProfile (ProMail,ProUserName, ProPicture, ProSavedTrees, ProDonated, ProPassword)
               VALUES(?,?,?,?,?,?)"; //,ProPicture,ProSavedTrees,ProDonated,ProPassword $ProUserName,$ProPicture,$ProSavedTrees,$ProDonated,$ProPassword')";
     $stmt = $db->prepare($sql);
    // $stmt->execute(array($ProMail,$ProUserName));
-   $stmt-> bind_param("ssssss",$ProMail, $ProUserName, $ProPicture, $ProSavedTrees, $ProDonated, $ProPassword);
+   $stmt-> bind_param("ssssss",/*$ProMail,*/ $obj, $ProUserName, $ProPicture, $ProSavedTrees, $ProDonated, $ProPassword);
     if(!$stmt) {
         die('error');
     } 
@@ -119,4 +73,3 @@ echo "judihui";
     ?>
 
     highlight_file
-    
